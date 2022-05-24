7b0b59984d1967fe593a910e1bcf74dc77401584
FranBondino <franbondino@gmail.com>
2022-05-24 17:31:33 -0300

    MASTER: resolucion de ejericcio 1

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,18 @@
+/*1. Variables y Operadores
+
+Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+ guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+ /* Concatenacion de strings */
+ var stringA = 'Mi nombre es'
+ var stringB = 'Francisco'
+ var stringSum = (stringA + stringB)
+
+ console.log (stringSum)
+
+ /* suma de largo de cada variable */
+ var stringLength = (stringA.length + stringB.length)
+
+ console.log (stringLength)
+
